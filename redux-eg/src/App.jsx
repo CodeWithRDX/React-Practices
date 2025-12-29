98ebd78ff270a2ded0e18ecd5cf5f0d3c48715ef
CodeWithRDX <Codewithrdx@gmail.com>
2025-12-29 22:39:38 +0530

    redux-eg

---
+++ redux-eg/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import {useDispatch,useSelector} from "react-redux"
+import { decrement, increment,incrementByAmount } from './redux/features/counterSlice'
+
+const App = () => {
+
+  const dispatch = useDispatch()
+  const count = useSelector((state)=>state.counter.value)
+
+  const [num, setNum] = useState(0)
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        dispatch(increment());
+      }}>Increment</button>
+      <button onClick={()=>{
+        dispatch(decrement());
+      }}>Decrement</button>
+
+
+      <input type="number" onChange={(e)=>{
+        setNum(Number(e.target.value));
+      }}/>
+      <button onClick={()=>{
+        dispatch(incrementByAmount(num));
+      }}>IncreseByAmount</button>
+    </div>
+  )
+}
+
+export default App
